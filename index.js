5dcd2044939c6ba10bf654747b577849cfe72571
gilbert <eleazarbiboum@gmail.com>
2022-03-15 18:42:29 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,48 @@
+function add(){
+   console .log(`'add()' is a valid function`)
+}
+
+function add(){
+    console .log(`'subtract()' is a valid function`)
+}
+function multiply(){
+   console .log(`'multiply()' is a valid function`)
+}    
+
+ function divide(){
+   console .log(`'divide()' is a valid function`)
+}
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
+
+function increment(n){
+    return ++n
+}
+
+function decrement(n){
+    return --n
+}
+
+function  makeInt(n){
+     parseInt(n, 10);
+     return parseInt(n,10)
+}
+function preserveDecimal(n,nnn){
+     parseFloat(n,nnn)
+return parseFloat(n,nnn)
+
+}
